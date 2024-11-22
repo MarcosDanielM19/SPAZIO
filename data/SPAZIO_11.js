cd8bdb3e0b77a94c9cbb5c627a4189a9fcb972cf
MarcosDanielM19 <danielmorales.e1906@gmail.com>
2024-11-21 19:06:57 -0600

    Add files via upload

---
+++ data/SPAZIO_11.js
@@ -0,0 +1,1 @@
+var json_SPAZIO_11 = {"type":"FeatureCollection","name":"SPAZIO_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"2"},"geometry":{"type":"Point","coordinates":[-99.15656271851536,19.424791051439797]}}]}
